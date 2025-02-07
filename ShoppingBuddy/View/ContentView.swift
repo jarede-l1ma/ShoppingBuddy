cf4958140701f25873bc746c1cdfc04225e2a0d3
Járede Lima <jarede.lima@a.ucb.br>
2025-02-06 22:45:30 -0300

    Extract properties from `ContentView` to create it owns classes

--- ShoppingBuddy/View/ContentView.swift
+++ ShoppingBuddy/View/ContentView.swift
@@ -37,116 +37,11 @@ struct ContentView: View {
     
     private var mainContentView: some View {
         VStack {
-            headerView
-            inputFieldsView
+            HeaderView(viewModel: viewModel)
+            InputFieldsView(viewModel: viewModel)
             listView
                 .padding(.top, 20)
-            totalView
-        }
-    }
-    
-    private var headerView: some View {
-        VStack {
-            Text("ShoppingBuddy")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.top, 20)
-            HStack {
-                Button(action: {
-                    withAnimation {
-                        viewModel.showInputFields.toggle()
-                    }
-                }) {
-                    Image(systemName: viewModel.showInputFields ? "xmark" : "plus")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(viewModel.showInputFields ? Color.red : Color.blue)
-                        .cornerRadius(10)
-                }
-                .padding(.leading, 20)
-                
-                Spacer()
-                
-                if !viewModel.showInputFields {
-                    Button(action: {
-                        viewModel.showDeleteAllAlert = true
-                    }) {
-                        Image(systemName: "trash")
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .frame(width: 50, height: 50)
-                            .background(Color.red)
-                            .cornerRadius(10)
-                    }
-                    .padding(.trailing, 20)
-                }
-            }
-            .padding(.top, 10)
-        }
-    }
-    
-    private var inputFieldsView: some View {
-        Group {
-            if viewModel.showInputFields {
-                VStack {
-                    TextField(TextfieldStrings.productName.localized, text: $viewModel.newItemName)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .overlay(
-                            viewModel.showDuplicateItemWarning ?
-                            Text(TextfieldStrings.itemAlreadyExists.localized)
-                                .font(.subheadline)
-                                .fontWeight(.medium)
-                                .foregroundColor(.white)
-                                .padding(8)
-                                .background(Color.red.opacity(0.9))
-                                .cornerRadius(8)
-                                .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
-                                .offset(y: -45)
-                                .transition(.scale.combined(with: .opacity))
-                            : nil
-                        )
-                        .modifier(ShakeEffect(animatableData: CGFloat(viewModel.showDuplicateItemWarning ? 1 : 0)))
-                        .animation(.interpolatingSpring(stiffness: 300, damping: 15), value: viewModel.showDuplicateItemWarning)
-                    
-                    TextField(TextfieldStrings.quantityItem.localized, text: $viewModel.newItemQuantity)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                    
-                    TextField(TextfieldStrings.unitValue.localized, text: $viewModel.newItemUnitPrice)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.decimalPad)
-                    
-                    Picker(TextfieldStrings.section.localized, selection: $viewModel.selectedSection) {
-                        ForEach(Sections.allCases, id: \.self) { section in
-                            Text(section.localized).tag(section)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    
-                    HStack {
-                        Button(viewModel.editingItem == nil ? ButtonsStrings.add.localized : ButtonsStrings.save.localized) {
-                            viewModel.editingItem == nil ? viewModel.addItem() : viewModel.updateItem()
-                        }
-                        .buttonStyle(PrimaryButtonStyle())
-                        Spacer()
-                        Button(action: { viewModel.clearForm() }) {
-                            Text(ButtonsStrings.clear.localized)
-                        }
-                        .buttonStyle(SecondaryButtonStyle())
-                        
-                        if viewModel.editingItem != nil {
-                            Button(ButtonsStrings.cancel.localized) {
-                                viewModel.clearForm()
-                                viewModel.editingItem = nil
-                            }
-                            .buttonStyle(SecondaryButtonStyle())
-                        }
-                    }
-                }
-                .padding(.horizontal)
-                .transition(.opacity.combined(with: .move(edge: .top)))
-            }
+            TotalView(viewModel: viewModel)
         }
     }
     
@@ -186,15 +81,5 @@ struct ContentView: View {
             onEdit: { viewModel.editItem(item) },
             onDelete: { viewModel.removeItem(item) }
         )
-    }
-    
-    private func sortItems(_ lhs: Item, _ rhs: Item) -> Bool {
-        lhs.name < rhs.name
-    }
-    
-    private var totalView: some View {
-        Text(ItemRowStrings.purchaseTotal.localized + "\(viewModel.formatCurrency(viewModel.totalPurchasePrice))")
-            .font(.headline)
-            .padding()
     }
 }
